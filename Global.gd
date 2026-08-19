8f49b357bc98d87262ab7bcb8f8d7b5a54b8074d
HT-AstroXcalibur5315 <hantharthonedra@gmail.com>
2026-08-19 16:30:51 -0700

    Adjustments to visuals for a more realistic experience

--- Global.gd
+++ Global.gd
@@ -13,4 +13,3 @@ func _process(_delta: float) -> void:
 
 var minigames_done = 0 #track how many minigames done
 var lives = 5 #track how many lives left, also effecting garlic appearing
-
--- Scenes/Scenes/title_scene.gd
+++ Scenes/Scenes/title_scene.gd
@@ -24,4 +24,3 @@ func _on_settings_pressed() -> void:
 func _on_quit_pressed() -> void:
 	pass # Replace with function body.
 	get_tree().quit()
-
